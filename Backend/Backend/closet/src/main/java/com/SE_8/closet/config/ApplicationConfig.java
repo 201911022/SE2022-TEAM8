02a6f22eb7b1c566104bfd73c191300d5eea2fd0
201911022 <101795921+201911022@users.noreply.github.com>
2022-06-01 23:46:14 +0900

    Delete Backend/Backend directory

--- Backend/Backend/closet/src/main/java/com/SE_8/closet/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.SE_8.closet.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = { /*"com.SE_8.closet.dao",  "com.SE_8.closet.service" */ })
-public class ApplicationConfig {
-}
